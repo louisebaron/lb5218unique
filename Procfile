5d28076a1436d7823ebeb76c2fe573aed1d8f896
louisebaron <louisebaron2000@hotmail.com>
2020-12-14 00:38:28 +0000

    HTML in GET response

--- Procfile
+++ Procfile
@@ -1,4 +1,4 @@
-web: java $JAVA_OPTS -jar ./webapp-runner-9.0.24.1.jar ${WEBAPP_RUNNER_OPTS} --port $PORT ./build/libs/lb5218unique-1.0-SNAPSHOT.war
+web: java $JAVA_OPTS -jar ./webapp-runner-9.0.24.1.jar ${WEBAPP_RUNNER_OPTS} --port $PORT./build/libs/lb5218unique-1.0-SNAPSHOT.war
 
 
 
--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -12,7 +12,25 @@ public class MyServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         resp.setContentType("text/html");
-        resp.getWriter().write("Hello World!");
+        resp.getWriter().write("<HTML>\n" +
+                "<HEAD>\n" +
+                "<TITLE>Your Title Here</TITLE>\n" +
+                "</HEAD>\n" +
+                "<BODY BGCOLOR=\"FFFFFF\">\n" +
+                "<CENTER><IMG SRC=\"clouds.jpg\" ALIGN=\"BOTTOM\"> </CENTER>\n" +
+                "<HR>\n" +
+                "<a href=\"http://somegreatsite.com\">Link Name</a>\n" +
+                "is a link to another nifty site\n" +
+                "<H1>This is a Header</H1>\n" +
+                "<H2>This is a Medium Header</H2>\n" +
+                "Send me mail at <a href=\"mailto:support@yourcompany.com\">\n" +
+                "support@yourcompany.com</a>.\n" +
+                "<P> This is a new paragraph!\n" +
+                "<P> <B>This is a new paragraph!</B>\n" +
+                "<BR> <B><I>This is a new sentence without a paragraph break, in bold italics.</I></B>\n" +
+                "<HR>\n" +
+                "</BODY>\n" +
+                "</HTML>");
 
         req.getServletPath(); // returns he servlet path of the url
     }
